01c26683a18a368e1754db4b82d247935e568417
Vojtěch Dobeš <me@vojtechdobes.com>
2014-08-13 16:41:13 +0200

    Added 'nextras.migrations.start' event
    Also, events 'complete', 'success' and 'fail' events are triggered only when
    at least migration is run.

--- src/MigrationsCommand.php
+++ src/MigrationsCommand.php
@@ -8,6 +8,7 @@ namespace DotBlue\Migrations;
 
 use Exception;
 use Kdyby\Events\EventManager;
+use Nette\Reflection\ClassType;
 use Nextras\Migrations\Controllers\ConsoleController;
 use Symfony\Component\Console;
 use Symfony\Component\Console\Input\InputArgument;
@@ -55,12 +56,58 @@ class MigrationsCommand extends Console\Command\Command
 
         // run controller
         try {
+            $processArguments = ClassType::from($this->controller)->getMethod('processArguments');
+            $processArguments->setAccessible(TRUE);
+            $processArguments->invoke($this->controller);
+            $registerGroups = ClassType::from($this->controller)->getMethod('registerGroups');
+            $registerGroups->setAccessible(TRUE);
+            $registerGroups->invoke($this->controller);
+
+            $runner = ClassType::from($this->controller)->getProperty('runner');
+            $runner->setAccessible(TRUE);
+            $runner = $runner->getValue($this->controller);
+            $orderResolver = ClassType::from($runner)->getProperty('orderResolver');
+            $orderResolver->setAccessible(TRUE);
+            $orderResolver = $orderResolver->getValue($runner);
+
+            $mode = ClassType::from($this->controller)->getProperty('mode');
+            $mode->setAccessible(TRUE);
+            $mode = $mode->getValue($this->controller);
+
+            $driver = ClassType::from($runner)->getProperty('driver');
+            $driver->setAccessible(TRUE);
+            $driver = $driver->getValue($runner);
+            $migrations = $driver->getAllMigrations();
+
+            $groups = ClassType::from($runner)->getProperty('groups');
+            $groups->setAccessible(TRUE);
+            $groups = $groups->getValue($runner);
+
+            $finder = ClassType::from($runner)->getProperty('finder');
+            $finder->setAccessible(TRUE);
+            $finder = $finder->getValue($runner);
+            $extensionsHandlers = ClassType::from($runner)->getProperty('extensionsHandlers');
+            $extensionsHandlers->setAccessible(TRUE);
+            $extensionsHandlers = $extensionsHandlers->getValue($runner);
+            $files = $finder->find($groups, array_keys($extensionsHandlers));
+
+            $toExecute = $orderResolver->resolve($migrations, $groups, $files, $mode);
+
+            if ($toExecute) {
+                $this->fireEvent('nextras.migrations.start');
+            }
+
             $this->controller->run();
-            $this->fireEvent('nextras.migrations.success');
-            $this->fireEvent('nextras.migrations.complete');
+
+            if ($toExecute) {
+                $this->fireEvent('nextras.migrations.success');
+                $this->fireEvent('nextras.migrations.complete');
+            }
         } catch (Exception $e) {
-            $this->fireEvent('nextras.migrations.fail');
-            $this->fireEvent('nextras.migrations.complete');
+            if (isset($toExecute) && $toExecute) {
+                $this->fireEvent('nextras.migrations.fail');
+                $this->fireEvent('nextras.migrations.complete');
+            }
             throw $e;
         }
     }
